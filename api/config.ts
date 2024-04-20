af2e2dab97028a5bc893bf4d7e5465d917d57463
Zhuravlev Petr <zeterfrost@gmail.com>
2024-04-20 15:20:49 +0600

    added and set up dependencies for api

---
+++ api/config.ts
@@ -0,0 +1,7 @@
+const config = {
+  mongoose: {
+    db: 'mongodb://localhost/wallet',
+  },
+};
+
+export default config;
--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import config from 'config';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(config.mongoose.db)],
   controllers: [AppController],
   providers: [AppService],
 })
