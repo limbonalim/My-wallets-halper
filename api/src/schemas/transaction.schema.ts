48532be00cdc33d6785208deed105ab42564e0e0
Zhuravlev Petr <zeterfrost@gmail.com>
2024-04-21 13:59:28 +0600

    created transaction schema

---
+++ api/src/schemas/transaction.schema.ts
@@ -0,0 +1,37 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Schema as MongooseSchema, Document } from 'mongoose';
+import { Wallet } from './wallet.schema';
+
+export enum TransactionType {
+  income = 'income',
+  outcome = 'outcome',
+}
+
+export enum TransactionCategory {
+  food = 'food',
+  clothes = 'clothes',
+  rent = 'rent',
+  paymentForServices = 'payment for services',
+  mobilePayment = 'mobile payment',
+  internetPayment = 'internet payment',
+  translations = 'translations',
+  salary = 'salary',
+  grocery = 'grocery',
+  other = 'other',
+}
+
+@Schema()
+export class Transaction {
+  @Prop({ required: true, enum: TransactionType })
+  type: string;
+  @Prop({ required: true, default: Date.now })
+  dataTime: string;
+  @Prop({ required: true, enum: TransactionCategory })
+  category: string;
+  @Prop({ required: true, ref: Wallet.name })
+  wallet: MongooseSchema.Types.ObjectId;
+}
+
+export const TransactionSchema = SchemaFactory.createForClass(Transaction);
+
+export type TransactionDocument = Transaction & Document;
