622a606eb2ddf777ed4309f703fd4ccfdc061387
Zhuravlev Petr <zeterfrost@gmail.com>
2024-04-21 13:52:57 +0600

    created user schema, service and controller

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,10 +3,31 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import config from 'config';
+import { User, UserSchema } from './schemas/user.schema';
+import { Wallet, WalletSchema } from './schemas/wallet.schema';
+import { Transaction, TransactionSchema } from './schemas/transaction.schema';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './auth/local.strategy';
+import { AuthService } from './auth/auth.service';
 
 @Module({
-  imports: [MongooseModule.forRoot(config.mongoose.db)],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(config.mongoose.db),
+    PassportModule,
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: Wallet.name, schema: WalletSchema },
+      { name: Transaction.name, schema: TransactionSchema },
+    ]),
+  ],
+  controllers: [AppController, UserController],
+  providers: [
+    AppService,
+    UserService,
+    AuthService,
+    LocalStrategy,
+  ],
 })
 export class AppModule {}
---
+++ api/src/user/user.controller.ts
@@ -0,0 +1,48 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Post,
+  Req,
+  UnprocessableEntityException,
+  UseGuards,
+} from '@nestjs/common';
+import mongoose from 'mongoose';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { CreateUserDto } from './create-user.dto';
+import { UserService } from './user.service';
+
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async registerUser(@Body() userData: CreateUserDto) {
+    try {
+      return await this.userService.register(userData);
+    } catch (error) {
+      if (error instanceof mongoose.Error.ValidationError) {
+        throw new UnprocessableEntityException(error);
+      }
+      if (error.code === 11000) {
+        throw new UnprocessableEntityException({
+          errors: { email: { message: 'This email is already registered' } },
+        });
+      }
+      throw error;
+    }
+  }
+
+  @UseGuards(AuthGuard('local'))
+  @Post('sessions')
+  async loginUser(@Req() req: Request) {
+    return req.user;
+  }
+
+  @Delete('sessions')
+  async logoutUser(@Req() request: Request) {
+    const headerValue = request.get('Authorization');
+    return await this.userService.logout(headerValue);
+  }
+}
