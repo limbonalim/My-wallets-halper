f39741ea9fde4ef59445a87296ab856a4e18a192
Zhuravlev Petr <zeterfrost@gmail.com>
2024-04-22 12:09:33 +0600

    refactored wallet controller and service

--- api/src/wallet/wallet.controller.ts
+++ api/src/wallet/wallet.controller.ts
@@ -4,29 +4,25 @@ import {
   Controller,
   Get,
   HttpStatus,
-  NotFoundException,
   Post,
   Req,
   Res,
   UnprocessableEntityException,
   UseGuards,
 } from '@nestjs/common';
+import mongoose from 'mongoose';
 import { WalletService } from './wallet.service';
 import { TokenAuthGuard } from 'src/auth/token-auth.guard';
 import { CreateWalletDto } from './create-wallet.dto';
-import mongoose from 'mongoose';
+
 
 @Controller('wallets')
 export class WalletController {
   constructor(private readonly walletService: WalletService) {}
   @UseGuards(TokenAuthGuard)
   @Get()
-  async getByUser(@Req() req: Request) {
-    const user = req.user;
-    if (!user) {
-      return new NotFoundException();
-    }
-    return this.walletService.getByUser(user.toString());
+  getByUser(@Req() req: Request) {
+    return this.walletService.getByUser(req.user.toString());
   }
 
   @UseGuards(TokenAuthGuard)
@@ -37,11 +33,10 @@ export class WalletController {
     @Req() req: Request,
   ) {
     try {
-      const user = req.user;
-      if (!user) {
-        return new NotFoundException();
-      }
-      const answer = await this.walletService.createOne(data, user.toString());
+      const answer = await this.walletService.createOne(
+        data,
+        req.user.toString(),
+      );
       return res.status(HttpStatus.CREATED).json(answer);
     } catch (error) {
       if (error instanceof mongoose.Error.ValidationError) {
--- api/src/wallet/wallet.service.ts
+++ api/src/wallet/wallet.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, Types } from 'mongoose';
 import { Wallet, WalletDocument } from 'src/schemas/wallet.schema';
@@ -11,13 +15,28 @@ export class WalletService {
   ) {}
 
   async getByUser(user: string) {
-    const userId = new Types.ObjectId(user);
-    const result = await this.walletModel.find({ user: userId });
-    return result;
+    let userId;
+    try {
+      userId = new Types.ObjectId(user);
+    } catch (e) {
+      throw new BadRequestException('Something was wrong!');
+    }
+
+    const answer = await this.walletModel.find({ user: userId });
+    if (!answer[0]) {
+      throw new NotFoundException('Wallet is not found');
+    }
+    return answer;
   }
 
   async createOne(walletDto: CreateWalletDto, user: string) {
-     const userId = new Types.ObjectId(user);
+    let userId;
+    try {
+      userId = new Types.ObjectId(user);
+    } catch (e) {
+      throw new BadRequestException('Something was wrong!');
+    }
+
     return await this.walletModel.create({
       name: walletDto.name,
       type: walletDto.type,
