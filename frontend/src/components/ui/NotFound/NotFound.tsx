045d29b48681c083fd336fabedb2088a4de3bacd
Zhuravlev Petr <zeterfrost@gmail.com>
2024-04-24 11:46:58 +0600

    added notFound page

---
+++ frontend/src/components/ui/NotFound/NotFound.tsx
@@ -0,0 +1,11 @@
+import { Typography } from '@mui/material';
+
+const NotFound = () => {
+  return (
+    <Typography variant="h2" sx={{ textAlign: 'center' }}>
+      Error 404! Not Found!
+    </Typography>
+  );
+};
+
+export default NotFound;
