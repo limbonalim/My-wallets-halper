e6a94bdd27e6054d8d53f427c16c743bfdfb85b4
Zhuravlev Petr <zeterfrost@gmail.com>
2024-04-24 11:52:58 +0600

    Added Routes in App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
+import NotFound from './components/ui/NotFound/NotFound';
+import Register from './feachers/Users/Register';
+import Login from './feachers/Users/Login';
+import Home from './feachers/Home/Home';
+import Wallets from './feachers/Wallets/Wallets';
+
+
 const App = () => {
   return (
     <>
-      <div>Hello, World</div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/wallets/:category" element={<Wallets/>} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   );
 };
