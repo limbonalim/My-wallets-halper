112ffcb841d4d397ec9886494f726f970e7a6f28
Zhuravlev Petr <zeterfrost@gmail.com>
2024-05-11 11:53:15 +0600

    added validation to create-user dto

--- api/src/user/create-user.dto.ts
+++ api/src/user/create-user.dto.ts
@@ -1,5 +1,11 @@
+import { IsEmail, IsString } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @IsEmail()
   email: string;
+  @IsString()
   password: string;
+  @IsString()
   displayName: string;
 }
