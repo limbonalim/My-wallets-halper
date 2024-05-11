adc6d656324c5e3eceabadee3a7687b5ba17a4f9
Zhuravlev Petr <zeterfrost@gmail.com>
2024-05-11 11:51:39 +0600

    added validation to create-transaction dto

--- api/src/transaction/create-transaction.dto.ts
+++ api/src/transaction/create-transaction.dto.ts
@@ -1,5 +1,14 @@
+import { IsDate, IsEnum } from 'class-validator';
+import {
+  TransactionCategory,
+  TransactionType,
+} from 'src/schemas/transaction.schema';
+
 export class CreateTransactionDto {
+  @IsEnum(TransactionType)
   type: string;
+  @IsDate()
   dataTime: string;
+  @IsEnum(TransactionCategory)
   category: string;
 }
