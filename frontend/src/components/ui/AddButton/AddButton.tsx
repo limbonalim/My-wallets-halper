f5046494f7779d20d87829ae0ba1ff0494185042
Zhuravlev Petr <zeterfrost@gmail.com>
2024-05-11 11:14:51 +0600

    create AddButton

---
+++ frontend/src/components/ui/AddButton/AddButton.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { PlusCircle } from '@phosphor-icons/react';
+import './AddButton.scss'
+
+interface Props {
+    onClick: () => void;
+}
+
+const AddButton: React.FC<Props> = ({onClick}) => {
+    return (
+      <button className="AddButton" onClick={onClick}>
+        <PlusCircle size={70} color="#030303" />
+      </button>
+    );
+};
+
+export default AddButton;
