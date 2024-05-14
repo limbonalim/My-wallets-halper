443fecfd6c0717f354455cecf78834489dc19adb
Zhuravlev Petr <zeterfrost@gmail.com>
2024-05-14 10:44:35 +0600

    added css to Waallet component

--- frontend/src/components/wallet/Wallet.tsx
+++ frontend/src/components/wallet/Wallet.tsx
@@ -1,18 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { IWallet } from '../../types';
 import './Wallet.scss';
+import {
+  CreditCard,
+  CurrencyBtc,
+  Pencil,
+  Wallet as WalletIcon,
+} from '@phosphor-icons/react';
+import WalletForm from '../AddWalletForm/WalletForm';
 
 const Wallet: React.FC<IWallet> = ({ _id, name, amount, type }) => {
   const path = `/transactions/${type}/${_id}`;
+  const [edit, setEdit] = useState<boolean>(false);
+  let icon;
+
+  switch (type) {
+    case 'crypto':
+      icon = <CurrencyBtc size={25} color="#030303" />;
+      break;
+    case 'wallet':
+      icon = <WalletIcon size={25} color="#030303" />;
+      break;
+    case 'card':
+      icon = <CreditCard size={25} color="#030303" />;
+      break;
+  }
+
+  const closeEditForm = () => {
+    setEdit(false);
+  };
+
   return (
-    <Link to={path}>
-      <Typography>
-        {' '}
-        {name} - {amount} KGC
-      </Typography>
-    </Link>
+    <div className="Wallet">
+      <Link to={path} className="Wallet__link">
+        {icon}
+        <Typography>
+          {name} - {amount} KGC
+        </Typography>
+      </Link>
+      <button
+        className="Base_batton Wallet__button"
+        onClick={() => setEdit((prev) => !prev)}
+      >
+        <Pencil size={32} color="#030303" />
+      </button>
+      <WalletForm
+        title="Edit"
+        show={edit}
+        type={type}
+        onClose={closeEditForm}
+        editData={{ _id, name, amount, type }}
+      />
+    </div>
   );
 };
 
