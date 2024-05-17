a7130244ea9aae3cbd0f9196d8b29890a68e3422
Zhuravlev Petr <zeterfrost@gmail.com>
2024-05-17 12:05:19 +0600

    fix bug in axios

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,6 +8,9 @@ import App from './App.tsx';
 import { persistor, store } from './app/store.ts';
 import theme from './theme.ts';
 import './index.scss'
+import { addInterceptors } from './axiosApi.ts';
+
+addInterceptors(store);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
